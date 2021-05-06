d3db1ba8d166a67da93a7a837e71b5b9bc370767
Heintsing <942689277@qq.com>
2021-05-06 22:53:47 +0800

    Pattern Try V1.1.0 加入了随机编码

--- PatternTry.py
+++ PatternTry.py
@@ -6,6 +6,7 @@ import scipy.io as io
 from matplotlib import pyplot as plt
 import scipy.io as scio
 import time
+import random
 
 Location_Channel = np.array([-1.20, 0, 1.37, 0.0, 0.0, 2.474])  # 设定源的位置 0.9 X正向西 Y正向北 上北下南左西右东 1.482 2.47-1.1
 com = 'COM6'
@@ -17,21 +18,56 @@ usrp, streamer, args, chan = SetUsrp()
 Communication.Print_Used_Com()
 Engine1 = Communication(com, 115200, 0.5)
 Engine1.Print_Name()
+t = []
+m = []
 
-Smn_hat = np.ones([24,24])
+
+Smn_hat_temp = np.ones([UnitNum,UnitNum])
+Smn_hat = np.ones([UnitNum,UnitNum])
 powerTemp = GetPower(streamer, args, chan)
-for row in range(UnitNum):
-    for column in range(UnitNum):
-        Smn_hat[row, column] = 1
+# for row in range(UnitNum):
+#     for column in range(UnitNum):
+#         Smn_hat[row, column] = 1
+#         power = GetPower(streamer, args, chan)
+#         print(power, 'dbm')
+#         if power<powerTemp:
+#             Smn_hat[row, column] = 0
+#         Pattern = Engine1.Image2hex(Smn_hat)
+#         Engine1.MetaDeploy(Pattern)
+try:
+    while 1:
+        index = np.random.randint(UnitNum * UnitNum, size=80)
+        # 在原编码的基础上 随机改变10%的bit
+        Smn_hat_temp[index // UnitNum, index % UnitNum] = 1
+        Pattern = Engine1.Image2hex(Smn_hat_temp)
+        Engine1.MetaDeploy(Pattern)
+        # 测试功率是否变大
         power = GetPower(streamer, args, chan)
         print(power, 'dbm')
-        if power<powerTemp:
-            Smn_hat[row, column] = 0
-        Pattern = Engine1.Image2hex(Smn_hat)
-        Engine1.MetaDeploy(Pattern)
+        if power > powerTemp:
+            Smn_hat = Smn_hat_temp
 
-PatternSavePath = r"F:\zht\CCC\CCCcloseData\analys\Smn_hat.mat"
-scio.savemat(PatternSavePath, {'Smnhat': Smn_hat})
+        powerTemp = power
+        Smn_hat_temp = Smn_hat
+
+        plt.clf()  # 清空画布上的所有内容
+        t_now = num
+        t.append(t_now)  # 模拟数据增量流入，保存历史数据
+        m.append(power)
+        plt.plot(t, m, '-r')
+        plt.draw()  # 注意此函数需要调用
+
+        plt.pause(0.001)
+except:
+    PatternSavePath = r"F:\zht\CCC\CCCcloseData\analys\Smn_hat.mat"
+    scio.savemat(PatternSavePath, {'Smnhat': Smn_hat})
+
+    fig = plt.figure()
+    plt.pcolor(Smn_hat, cmap='jet', edgecolors='k', linewidths=0.4)
+    plt.colorbar(shrink=.83)
+    plt.xticks()
+    plt.yticks()
+    plt.show()
 
 
 
---
+++ test.py
@@ -0,0 +1,28 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+Pattern = np.ones([24, 24])
+
+
+fig = plt.figure()
+plt.pcolor(Pattern, cmap='jet', edgecolors='k', linewidths=0.4)
+plt.colorbar(shrink=.83)
+plt.xticks()
+plt.yticks()
+plt.show()
+
+fig, (ax0, ax1) = plt.subplots(1, 2)
+Z = - MS.Smn_hat
+Z = np.flip(Z, axis=1)
+c = ax0.pcolor(Z, cmap='jet')
+c = ax1.pcolor(Z, edgecolors='k', linewidths=0.4)
+fig.tight_layout()
+plt.show()
+
+fig2,ax0 = plt.figure()
+plt.pcolor(Z.T, cmap='jet')
+plt.colorbar(shrink=.83)
+plt.xticks()
+plt.yticks()
+plt.show()
